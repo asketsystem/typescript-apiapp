b6c025d140746e4730af4c2a31f8ccefd831186f
asketsystem <asketsystem1@gmail.com>
2022-01-31 13:44:25 +0000

    I'm getting the data from jsonplaceholder and for that I will do the API call to the posts endpoint in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import './App.css';
 import Header from './components/Header';
-import { useState } from 'react';
+import {useEffect, useState } from 'react';
 import ListNotes from './components/ListNotes';
 import { Note } from './models/note.model';
 
 function App() {
- const [notes, setNotes] = useState<Note[]>([
-   {
-     id: "1",
-     title: "Demo Title",
-     body: "This is just a demo content",
-   }
- ]);
+ const [notes, setNotes] = useState<Note[]>([]);
+
+ useEffect(() => {
+   fetch('https://jsonplaceholder.typicoder.com/posts')
+      .then(response => response.json())
+      .then(data => setNotes(data))
+      .catch(err => console.log(err));
+ },[])
 
  return (
    <>
